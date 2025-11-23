499c97250be21978e1474b6fd23592d2146bee5b
Xinwu Ye <1010647987@qq.com>
2025-11-23 22:46:30 +0800

    hp

--- configs/molecule/pdbbind_ligand_ptv3_vae.py
+++ configs/molecule/pdbbind_ligand_ptv3_vae.py
@@ -2,7 +2,8 @@ _base_ = ["../_base_/default_runtime.py"]
 
 
 # Misc
-batch_size = 1024
+# batch_size = 1024
+batch_size = 64
 num_worker = 8
 mix_prob = 0.0
 empty_cache = False
@@ -75,8 +76,8 @@ model = dict(
 )
 
 # Optimizer & scheduler
-epoch = 50
-eval_epoch = 10
+# epoch = 200
+# eval_epoch = 10
 # optimizer = dict(type="AdamW", lr=0.0005, weight_decay=0.01)
 # scheduler = dict(
 #     type="OneCycleLR",
@@ -87,16 +88,18 @@ eval_epoch = 10
 #     final_div_factor=1000.0,
 # )
 # param_dicts = [dict(keyword="block", lr=0.0005)]
-optimizer = dict(type="AdamW", lr=0.001, weight_decay=0.01)
+epoch = 200
+eval_epoch = 200
+optimizer = dict(type="AdamW", lr=0.0001, weight_decay=0.01)
 scheduler = dict(
     type="OneCycleLR",
-    max_lr=[0.001, 0.001],
+    max_lr=[0.0001, 0.00005],
     pct_start=0.05,
     anneal_strategy="cos",
     div_factor=10.0,
     final_div_factor=1000.0,
 )
-param_dicts = [dict(keyword="block", lr=0.001)]
+param_dicts = [dict(keyword="block", lr=0.00005)]
 
 # Dataset
 dataset_type = "MoleculeDataset"
